c2ee650aa1cad607925769ae4d2ea385114efea3
Oghenemarho Orukele <orukele.dev@gmail.com>
2023-04-25 18:57:26 +0200

    testing pull request

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,23 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub fn divide(left: usize, right: usize) -> usize {
+    left / right
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn add_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn divide_works() {
+        let result = divide(2, 2);
+        assert_eq!(result, 1);
     }
 }
