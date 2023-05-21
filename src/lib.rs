eac999378bae55e647b9166d73c3886ba39cc8de
Oghenemarho Orukele <orukele.dev@gmail.com>
2023-05-21 17:41:02 +0200

    added client and error files

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,2 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-pub fn divide(left: usize, right: usize) -> usize {
-    left / right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn add_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn divide_works() {
-        let result = divide(2, 2);
-        assert_eq!(result, 1);
-    }
-}
+pub mod client;
+pub mod error;
